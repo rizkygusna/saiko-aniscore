ac8d4f9ee5f7671a892d859aa0f7fb10312e7c9a
rizkygusna <gusnafarid@gmail.com>
2022-07-07 12:21:17 +0700

    change query to spring season

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { FaStar } from 'react-icons/fa';
 import Card from './components/Card';
 
 function App() {
-  const [season, setSeason] = useState('Summer');
+  const [season, setSeason] = useState('spring');
   const [year, setYear] = useState(0);
   const [animes, setAnimes] = useState([]);
 
@@ -17,9 +17,9 @@ function App() {
 
   const getSeasonNow = async () => {
     try {
-      const res = await fetch('https://api.jikan.moe/v4/seasons/now');
+      const res = await fetch('https://api.jikan.moe/v4/seasons/2022/spring');
       const jsonRes = await res.json();
-      const filteredData = jsonRes.data.filter((anime) => anime.score > 7);
+      const filteredData = jsonRes.data.filter((anime) => anime.score > 7.5);
       setAnimes(filteredData);
       setSeason(jsonRes.data[0].season);
     } catch (error) {
@@ -37,7 +37,7 @@ function App() {
     <div className='App'>
       <header className='Header'>
         <h1>Saiko Aniscore</h1>
-        <p>Find the best rated anime score of the season.</p>
+        <p>Find the best rated anime of the season.</p>
       </header>
       <main>
         <section>
